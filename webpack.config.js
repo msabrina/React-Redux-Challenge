13e85cd88520d54f2caa935cdbc47b1d3085ec80
Sabrina Mesa <msabrina88@gmail.com>
2017-03-05 23:43:14 -0500

    Created file structure with some redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,66 @@
+'use strict'
 const webpack           = require('webpack');
 const path              = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const htmlTemplate      = require('html-webpack-template');
 
 const BUILD_DIR         = path.resolve(__dirname, 'dist');
-const APP_DIR           = path.resolve(__dirname, 'src/client/app');
+const APP_DIR           = path.resolve(__dirname, 'src');
 
-const config = {
-  entry:  `${APP_DIR}/main.js`,
+
+module.exports = {
+  entry: `${APP_DIR}/index.js`,
   output: {
-    path:     BUILD_DIR,
+    path: BUILD_DIR,
     filename: '/js/[name].js',
   },
-  cache:   true,
-  debug:   true,
+  cache: true,
+  debug: true,
   devtool: 'eval-source-map',
-  stats:   {
-    colors:  true,
-    reasons: true,
+  stats: {
+    colors: true,
+    reasons: true
   },
-
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-      },
-    }),
     new HtmlWebpackPlugin({
-      title:      'Tasks',
-      xhtml:      true,
-      inject:     false,
-      template:   htmlTemplate,
-      appMountId: 'container',
+      title: 'ReactJS Hello World',
+      xhtml: true,
+      inject: false,
+      template: require('html-webpack-template'),
+      appMountId: 'root-container'
     }),
     new ExtractTextPlugin('/css/[name].css', {
-      allChunks: true,
-    }),
+      allChunks: true
+    })
   ],
 
-  module: {
+  module : {
+    include: path.join(__dirname, 'src'),
     loaders: [
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-      { test: /\.(png|gif|jpg)$/, loader: 'file-loader?name=/images/[name].[ext]' },
-      { test: /\.ico$/, loader: 'file-loader?name=/[name].[ext]' },
-      { test: /\.jsx?$/, loader: 'babel' },
       {
-        test:   /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=100&mimetype=application/font-woff&name=/fonts/[name].[ext]',
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
       },
       {
-        test:   /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=100&mimetype=application/octet-stream&name=/fonts/[name].[ext]',
+        test: /\.svg$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
       },
       {
-        test:   /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader?name=/fonts/[name].[ext]',
+        test: /\.gif$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
       },
       {
-        test:   /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=100&mimetype=image/svg+xml&name=/fonts/[name].[ext]',
+        test: /\.jpg$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
       },
-    ],
-  },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel'
+      },
+      {
+        test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=/fonts/[name].[ext]'
+      }
+    ]
+  }
 };
-
-if (process.env &&
-  process.env.NODE_ENV &&
-  process.env.NODE_ENV === 'production') {
-  const prodPlugins = [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: true,
-      },
-      output: {
-        comments: false,
-      },
-    }),
-    new webpack.optimize.CommonsChunkPlugin('/js/common.js'),
-  ];
-
-  config.plugins = config.plugins.concat(prodPlugins);
-
-  config.cache = false;
-  config.debug = false;
-  config.devtool = undefined;
-}
-
-module.exports = config;
